a95699a113303e985deea2189aa35699f470beac
biniyam2019 <110065160+biniyam2019@users.noreply.github.com>
2023-02-11 17:58:05 +0530

    asfelagr

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Movie from "../component/Movie";
 const inter = Inter({ subsets: ["latin"] });
 import Slider from "../component/Slider";
 import Footer from "../component/Footer";
+
 export default function Home() {
   return (
     <>
@@ -16,7 +17,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
+    
       <Slider />
       <Movie />
     </>
